c18bffb5767c13f6ef98c57612f2fe6cec926bd9
Cesar <calfonsomora@gmail.com>
2024-10-29 11:29:05 -0500

    Cambios en el Diseño de Terminales.

--- src/app/administrador/componentes/menu/menu.component.ts
+++ src/app/administrador/componentes/menu/menu.component.ts
@@ -18,6 +18,13 @@ export class MenuComponent implements OnInit {
   isCollapsed = false;
   desplegado = true
 
+  inicioSesion: boolean = false
+  inicioVigia2: boolean = false
+
+  nombrePrimerModulo: string = '';
+  nombreModuloSeleccionado: string = '';
+  primerModulo?: string; // Inicializar primerModulo como opcional
+
   constructor(
     private servicioLocalStorage: ServicioLocalStorage,
     private servicioAutenticacion: AutenticacionService,
@@ -27,8 +34,14 @@ export class MenuComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const inicioSesion = JSON.parse(localStorage.getItem('inicio-sesion') || 'false');
+    if(inicioSesion){this.inicioSesion = inicioSesion}
+    const inicioVigia2 = JSON.parse(localStorage.getItem('inicio-vigia2') || 'false');
+    if(inicioVigia2){this.inicioVigia2 = inicioVigia2}
+
     this.rol = this.servicioLocalStorage.obtenerRol()
     this.usuario = this.servicioLocalStorage.obtenerUsuario()
+    this.setNombreModuloInicial();
   }
 
   public abrir(): void {
@@ -62,5 +75,44 @@ export class MenuComponent implements OnInit {
   navegarAlSubmodulo(submodulo: Submodulo) {
     this.imprimirRuta(submodulo)
     this.router.navigateByUrl(`/administrar${submodulo.ruta}`)
+  }
+
+  private setNombreModuloInicial() {
+    const currentRoute = this.router.url;
+
+    if (this.rol && this.rol.modulos && this.rol.modulos.length > 0) {
+      // Buscar un módulo o submódulo que coincida con la ruta actual
+      let moduloActual = this.rol.modulos.find(modulo =>
+        currentRoute.includes(`/administrar${modulo.ruta}`)
+      );
+
+      if (!moduloActual) {
+        // Si no hay coincidencia en la ruta, establece el primer módulo como el seleccionado por defecto
+        moduloActual = this.rol.modulos[0];
+      }
+
+      this.nombreModuloSeleccionado = moduloActual.nombre;
+    }
+  }
+
+  // Método para cambiar el módulo seleccionado
+  seleccionarModulo(modulo: string) {
+    this.nombreModuloSeleccionado = modulo;
+  }
+
+  getPrimeraRutaModulo(): string {
+    if (this.rol && this.rol.modulos && this.rol.modulos.length > 0) {
+      // Obtiene el primer módulo disponible
+      const primerModulo = this.rol.modulos[0];
+
+      // Muestra el nombre del primer módulo en la consola
+
+      this.nombrePrimerModulo = primerModulo.nombre; // Asigna el nombre del primer módulo a la variable
+      // Redirige al primer módulo disponible con el prefijo "administrar"
+      return `/administrar${primerModulo.ruta}`;
+    } else {
+      // Si no hay módulos disponibles, redirige a una página de error o la ruta que prefieras
+      return '/error'; // Ajusta esta ruta según tu app o añade manejo adicional
+    }
   }
 }
